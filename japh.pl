14f405692a9578115eb30375ba8e04af40335d39
Charlie Reynolds <reynoldscem@gmail.com>
2016-12-03 19:42:02 +0000

    japh

--- japh.pl
+++ japh.pl
@@ -1,1 +1,1 @@
-$_='';eval("join('',(map{chr($_)}(grep/\\S/,split(/(...)/,'074117115116032097110111116104101114032080101114108032104097099107101114044'))))")=~/.*/;print$&;
+use MIME::Base64;$_='';(eval decode_base64('am9pbignJywobWFwe2NocigpfShncmVwL1xTLyxzcGxpdCgvKC4uLikvLCcwNzQxMTcxMTUxMTYwMzIwOTcxMTAxMTExMTYxMDQxMDExMTQwMzIwODAxMDExMTQxMDgwMzIxMDQwOTcwOTkxMDcxMDExMTQwNDQnKSkpKQ=='))=~/.*/;print$&;
